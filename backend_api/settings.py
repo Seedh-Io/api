b70e23472e5c0dd926c5d65c6afe3bd3928c2aba
manavchawla2012 <manavchawla2012@gmail.com>
2023-04-10 00:34:14 +0530

    Update settings.py

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-avua7=duw_qzx86+4@2n#vvgn11t$237kqwrr(loj3az7$&!)s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,8 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "seedh",
-        'USER': "manav",
-        'PASSWORD': "manav",
+        'USER': "",
+        'PASSWORD': "",
         'HOST': "localhost",
         'PORT': "5432",
     }
@@ -227,8 +227,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'seedh.acc@gmail.com'
-EMAIL_HOST_PASSWORD = 'znrkvjipvsicitzi'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'backend_api.helpers.custom_exception_helper.custom_exception_handler',
